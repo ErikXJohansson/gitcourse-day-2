f32727c7beecff1205868ba8c51e15352b406abf
ErikXJohansson <Erik.X.Johansson@hm.com>
2021-02-05 11:08:13 +0100

    Decrement feature

--- src/january/Main.java
+++ src/january/Main.java
@@ -6,10 +6,16 @@ public class Main {
         int num = 10;
         increment(num);
         System.out.println(num);
+        decrement(num);
+        System.out.println(num);
 
     }
 
     public static int increment(int number) {
-        return number+1;
+        return ++number;
+    }
+
+    public static int decrement(int number) {
+        return --number;
     }
 }
