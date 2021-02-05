5132bfa8d2a44f2a55e92eb0830501243262f2b7
ErikXJohansson <Erik.X.Johansson@hm.com>
2021-02-05 09:57:03 +0100

    print figure

---
+++ src/january/Main.java
@@ -0,0 +1,15 @@
+package january;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num = 10;
+        increment(num);
+        System.out.println(num);
+
+    }
+
+    public static int increment(int number) {
+        return number+1;
+    }
+}
